4eae0b21ba54bfeebcb9cd2a903d10e46bb00c57
ewasion137 <formanban94@gmail.com>
2026-03-15 17:10:38 +0200

    upd 3

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #define VIEW_MATRIX_OFFSET 0x230EF20
 
@@ -10,16 +11,23 @@ void ModifyViewMatrix() {
 
     float* matrix = (float*)(client_base + VIEW_MATRIX_OFFSET);
 
+    // Добавь проверку на валидность адреса
+    if (IsBadReadPtr(matrix, 64)) return; 
+
     DWORD old_protect;
     VirtualProtect(matrix, 64, PAGE_EXECUTE_READWRITE, &old_protect);
 
-    matrix[0] = 1.5f; // Растягиваем X
+    matrix[0] = 1.5f; // Пытаемся принудительно держать 1.5
 
     VirtualProtect(matrix, 64, old_protect, &old_protect);
 }
 
 // Поток, который будет ждать нажатия клавиши
 DWORD WINAPI MainThread(LPVOID lpParam) {
+    AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    printf("Debug: Client Base = %p\n", (void*)GetModuleHandleA("client.dll"));
+    printf("Debug: Matrix Address = %p\n", (void*)((uintptr_t)GetModuleHandleA("client.dll") + VIEW_MATRIX_OFFSET));
     while (1) {
         if (GetAsyncKeyState(VK_INSERT) & 1) {
              ModifyViewMatrix();
